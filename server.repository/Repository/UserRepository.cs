0e7997b15cf5a9a196ff7a1cf69011053c4961c1
Hit Bambhroliya <hit.bambhroliya@etatvasoft.com>
2024-06-11 16:33:18 +0530

    repo pattern & database schema

---
+++ server.repository/Repository/UserRepository.cs
@@ -0,0 +1,14 @@
+﻿using server.repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server.repository.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+
+    }
+}
--- server/obj/Release/net8.0/server.AssemblyInfo.cs
+++ server/obj/Release/net8.0/server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cab0b106cd853f2d6755ab593f405c7f275ea074")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
